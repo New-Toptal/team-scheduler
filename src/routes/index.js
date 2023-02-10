a3fb7f70778b6821b51dea64fe6fe31b2df01e1b
Rafael Brouwer <happinies11@gmail.com>
2023-02-10 15:04:49 +0900

    Add routes

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+const Routes = () => {
+    return (
+        <Router>
+            <Route path='path'/>
+            <Route path=''/>
+
+        </Router>
+    )
+}
+export default Routes;
