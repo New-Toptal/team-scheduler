3f083591843a478755d514a179bdb2a0d5a03fab
Rafael Brouwer <happinies11@gmail.com>
2023-02-12 16:31:20 +0900

    feature/ts003:Complete Layout

--- src/pages/Auth/Home/index.tsx
+++ src/pages/Auth/Home/index.tsx
@@ -30,15 +30,15 @@ const Home: React.FC = () => {
                 <span className='w-full  h-1 rounded-sm border-x-purple-200 border-2  my-4'>
 
                 </span>
-                <div className='flex justify-between w-full max-md:w-full max-md:h-full h-full space-y-3'>
+                <div className='flex justify-between w-full max-md:w-full max-md:h-full h-full'>
                     <div className='flex w-full h-full'>
                         <div className='flex justify-between items-sat flex-col w-1/6 px-2 py-4 mx-3 border-2 rounded-md'>
 
-                            <div className='md:my-2 w-full h-40 bg-red-300'>
-                                <div className='flex justify-center'>
+                            <div className='md:my-1 w-full h-40 rounded-md shadow-xl bg-blue-300'>
+                                <div className='flex justify-center py-4'>
                                     <h1>Calendar</h1>
                                     <div className='grid'>
-                                        
+
                                     </div>
                                 </div>
 
@@ -69,14 +69,14 @@ const Home: React.FC = () => {
                             <div className='mt-3'>
                                 <h3 className='border-dotted border-b border-gray-200'>My Task</h3>
                                 <div className='flex w-full justify-start hover:shadow-sm'>
-                                        <div className='px-4 mt-3'>
-                                            <ul className='space-y-3'>
-                                                <li className='border-b border-gray-200'>
-                                                    <button className='bg-gradient-to-tr from-amber-500-500 to-yellow-400 w-[20] h-[20] text-center text-blue-400'>Task1</button>
-                                                </li>
-                                                <li className='border-b border-gray-200'>Task2</li>
-                                                <li className='border-b border-gray-200' >Task3</li>
-                                            </ul>
+                                    <div className='px-4 mt-3'>
+                                        <ul className='space-y-3'>
+                                            <li className='border-b border-gray-200'>
+                                                <button className='bg-gradient-to-tr from-amber-500-500 to-yellow-400 w-[20] h-[20] text-center text-blue-400'>Task1</button>
+                                            </li>
+                                            <li className='border-b border-gray-200'>Task2</li>
+                                            <li className='border-b border-gray-200' >Task3</li>
+                                        </ul>
                                     </div>
 
                                 </div>
@@ -85,10 +85,21 @@ const Home: React.FC = () => {
                         </div>
                         <main className='grow-0 w-full h-full'>
 
+
                         </main>
                         {/* Task Box */}
-                        <div className='w-1/6 m-2'>
-                            <div className="flex w-full h-full shadow-lg   bg-transparent bg-white-300">
+                        <div className='w-1/12 m-2'>
+                            <div className="flex flex-col w-full h-full shadow-lg   bg-transparent bg-white-300 px-2 py-6">
+                                {/* Add Task */}
+                                <div className='mx-auto'>
+                                    <button className='w-[10] h-[10] text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'>Add Task</button>
+
+                                </div>
+                                {/* Add User */}
+                                <div>
+                                    <button className='w-[10] h-[10] text-white bg-pink-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'>Add User</button>
+
+                                </div>
 
                             </div>
                         </div>
