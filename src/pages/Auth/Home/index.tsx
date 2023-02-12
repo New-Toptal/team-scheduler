26a3e7b75496edd88e4fd58c1bab1cba3ba00945
Rafael Brouwer <happinies11@gmail.com>
2023-02-12 16:11:35 +0900

    feature/ts003:Complete left Laout

--- src/pages/Auth/Home/index.tsx
+++ src/pages/Auth/Home/index.tsx
@@ -30,42 +30,57 @@ const Home: React.FC = () => {
                 <span className='w-full  h-1 rounded-sm border-x-purple-200 border-2  my-4'>
 
                 </span>
-                <div className='flex justify-between w-full h-full'>
+                <div className='flex justify-between w-full max-md:w-full max-md:h-full h-full space-y-3'>
                     <div className='flex w-full h-full'>
-                        <div className='flex justify-start items-sat flex-col w-1/6 px-2 py-4 mx-3 border-2 rounded-md'>
+                        <div className='flex justify-between items-sat flex-col w-1/6 px-2 py-4 mx-3 border-2 rounded-md'>
 
                             <div className='md:my-2 w-full h-40 bg-red-300'>
+                                <div className='flex justify-center'>
+                                    <h1>Calendar</h1>
+                                    <div className='grid'>
+                                        
+                                    </div>
+                                </div>
 
                             </div>
                             {/* User List */}
-                            <div className='w-full flex flex-col justify-between space-y-2 font-mono px-6' >
-                                <div className='grid grid-cols-2 items-center'>
-                                    <span className='place-self-start'>name1</span>
-                                    <input className='place-self-center self-center' type="checkbox" name="" id="" />
-                                </div>
-                                <div className='grid grid-cols-2 items-center'>
-                                    <span className='place-self-start'>name2</span>
-                                    <input className='place-self-center self-center' type="checkbox" name="" id="" />
-                                </div>
-                                <div className='grid grid-cols-2 items-center'>
-                                    <span className='place-self-start'>name3</span>
-                                    <input className='place-self-center self-center' type="checkbox" name="" id="" />
-                                </div>
-                                <div className='grid grid-cols-2 items-center'>
-                                    <span className='place-self-start'>names4</span>
-                                    <input className='place-self-center self-center' type="checkbox" name="" id="" />
+                            <div>
+                                <h1 className='mb-3 border-dotted border-b border-gray-200'>User List</h1>
+                                <div className='w-full flex flex-col justify-between space-y-2 font-mono px-6' >
+                                    <div className='grid grid-cols-2 items-center'>
+                                        <span className='place-self-start'>name1</span>
+                                        <input className='place-self-center self-center' type="checkbox" name="" id="" />
+                                    </div>
+                                    <div className='grid grid-cols-2 items-center'>
+                                        <span className='place-self-start'>name2</span>
+                                        <input className='place-self-center self-center' type="checkbox" name="" id="" />
+                                    </div>
+                                    <div className='grid grid-cols-2 items-center'>
+                                        <span className='place-self-start'>name3</span>
+                                        <input className='place-self-center self-center' type="checkbox" name="" id="" />
+                                    </div>
+                                    <div className='grid grid-cols-2 items-center'>
+                                        <span className='place-self-start'>names4</span>
+                                        <input className='place-self-center self-center' type="checkbox" name="" id="" />
+                                    </div>
                                 </div>
                             </div>
-                            
                             {/*My Schedule  */}
+                            <div className='mt-3'>
+                                <h3 className='border-dotted border-b border-gray-200'>My Task</h3>
+                                <div className='flex w-full justify-start hover:shadow-sm'>
+                                        <div className='px-4 mt-3'>
+                                            <ul className='space-y-3'>
+                                                <li className='border-b border-gray-200'>
+                                                    <button className='bg-gradient-to-tr from-amber-500-500 to-yellow-400 w-[20] h-[20] text-center text-blue-400'>Task1</button>
+                                                </li>
+                                                <li className='border-b border-gray-200'>Task2</li>
+                                                <li className='border-b border-gray-200' >Task3</li>
+                                            </ul>
+                                    </div>
 
-                            <div className='flex w-full justify-start'>
-                                <div>
-                                <h3 className='mx-auto'>My Task</h3>
                                 </div>
-
                             </div>
-
 
                         </div>
                         <main className='grow-0 w-full h-full'>
