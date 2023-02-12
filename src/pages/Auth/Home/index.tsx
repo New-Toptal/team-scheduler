d698322ea0cadae014083e0fb1be3539a279537c
Rafael Brouwer <happinies11@gmail.com>
2023-02-12 15:44:44 +0900

    feature/ts003:Add Layout

--- src/pages/Auth/Home/index.tsx
+++ src/pages/Auth/Home/index.tsx
@@ -4,58 +4,86 @@ const Home: React.FC = () => {
     return (
         <div className='box-border w-screen h-screen'>
             <div className="flex flex-col  items-center box-border w-full h-full px-4 py-6">
-                <div className='flex justify-center items-center  h-50 w-full'>
-                    <div className='flex-shrink'>
+                <header className='w-full grid grid-cols-3 h-10 items-center'>
+                    <div className='items-center'>
                         <h1>Today's schedule</h1>
                     </div>
-                    <div className='' >
-                        <button className='rounded-full border-red-700'>
-                            ok
-                        </button>
+                    <div className='flex justify-center items-center'>
+                        Tittle
                     </div>
-                </div>
+                    <div className='flex justify-end space-x-2'>
+
+                        <div className="right">
+
+                            <div
+                                className="bg-gradient-to-tr from-yellow-500 to-yellow-400 w-[20] h-[20]  rounded-full shadow-xl shadow-yellow-400 border-white   border-2  flex justify-center items-center ">
+                                <button className="text-white text-md px-2">OK</button>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex">
+                                img
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <span className='w-full  h-1 rounded-sm border-x-purple-200 border-2  my-4'>
+
+                </span>
                 <div className='flex justify-between w-full h-full'>
                     <div className='flex w-full h-full'>
-                        <div className='flex justify-start items-center flex-col w-1/6 px-2 py-4 mx-3 border-2 rounded-md'>
-                                <div className='rounded-md'>
-                                    <div className='w-40 h-40 bg-red-300'>
+                        <div className='flex justify-start items-sat flex-col w-1/6 px-2 py-4 mx-3 border-2 rounded-md'>
 
-                                    </div>
-                                    {/* User List */}
-                                    <div className='w-full flex flex-col space-y-2 font-mono px-4' >
-                                        <div className='grid grid-cols-2 items-center'>
-                                            <span className=''>name1</span>
-                                            <input className='place-self-start self-center' type="checkbox" name="" id="" />
-                                        </div>
-                                        <div className='grid grid-cols-2 items-center'>
-                                            <span className=''>name2</span>
-                                            <input className='place-self-start self-center' type="checkbox" name="" id="" />
-                                        </div>
-                                        <div className='grid grid-cols-2 items-center'>
-                                            <span className=''>name3</span>
-                                            <input className='place-self-start self-center' type="checkbox" name="" id="" />
-                                        </div>
-                                        <div className='grid grid-cols-2 items-center'>
-                                            <span className=''>name4</span>
-                                            <input className='place-self-start self-center' type="checkbox" name="" id="" />
-                                        </div>
-                                    </div>
+                            <div className='md:my-2 w-full h-40 bg-red-300'>
 
                             </div>
-                            <div>
-                                sdf
+                            {/* User List */}
+                            <div className='w-full flex flex-col justify-between space-y-2 font-mono px-6' >
+                                <div className='grid grid-cols-2 items-center'>
+                                    <span className='place-self-start'>name1</span>
+                                    <input className='place-self-center self-center' type="checkbox" name="" id="" />
+                                </div>
+                                <div className='grid grid-cols-2 items-center'>
+                                    <span className='place-self-start'>name2</span>
+                                    <input className='place-self-center self-center' type="checkbox" name="" id="" />
+                                </div>
+                                <div className='grid grid-cols-2 items-center'>
+                                    <span className='place-self-start'>name3</span>
+                                    <input className='place-self-center self-center' type="checkbox" name="" id="" />
+                                </div>
+                                <div className='grid grid-cols-2 items-center'>
+                                    <span className='place-self-start'>names4</span>
+                                    <input className='place-self-center self-center' type="checkbox" name="" id="" />
+                                </div>
                             </div>
-                        </div>
-                        <div className='grow-0 w-full h-full'>
+                            
+                            {/*My Schedule  */}
+
+                            <div className='flex w-full justify-start'>
+                                <div>
+                                <h3 className='mx-auto'>My Task</h3>
+                                </div>
+
+                            </div>
+
 
                         </div>
-                        <div className='w-1/8 m-2'>
-                            This is box
+                        <main className='grow-0 w-full h-full'>
+
+                        </main>
+                        {/* Task Box */}
+                        <div className='w-1/6 m-2'>
+                            <div className="flex w-full h-full shadow-lg   bg-transparent bg-white-300">
+
+                            </div>
                         </div>
                     </div>
                 </div>
-                <footer>
-                    ok
+                <footer className='w-full'>
+                    <div className=''>
+                        ok
+                    </div>
+
                 </footer>
             </div>
         </div>
