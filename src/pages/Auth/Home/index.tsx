1c7543243548837043fa36748c1eccf6158c199b
Rafael Brouwer <happinies11@gmail.com>
2023-02-12 13:35:58 +0900

    featrue/ts002:add layout

--- src/pages/Auth/Home/index.tsx
+++ src/pages/Auth/Home/index.tsx
@@ -1,10 +1,51 @@
 import React from 'react'
-import { Eventcalendar, setOptions } from '@mobiscroll/react';
+import { Eventcalendar, getJson, toast, MbscCalendarEvent, MbscEventcalendarView } from '@mobiscroll/react';
 
-const Home = () => {
-    return(
-        <div>
-            <Eventcalendar theme='ios' themeVariant='light' clickToCreate={true} dragToCreate={true} />
+const Home: React.FC = () => {
+
+    const view = React.useMemo<MbscEventcalendarView>(() => {
+        return {
+            schedule: { type: 'week' }
+        };
+    }, []);
+
+    return (
+        <div className='box-border w-screen h-screen'>
+            <div className="flex flex-col  items-center box-border w-full h-full px-4 py-6">
+                <div className='flex justify-center items-center  h-50 w-full'>
+                    <div className='flex-shrink'>
+                            <h1>Today's schedule</h1>
+                    </div>
+                    <div className='' >
+                            <button className='rounded-full border-red-700'>
+                                ok
+                            </button>
+                    </div>
+                </div>
+                <div className='flex justify-between w-full h-full'>
+                    <div className='flex w-full h-full'>
+                        <div className='flex justify-start items-center flex-col w-1/6 mx-3 border-2 border-spacing-2  rounded-md'>
+                            <div>
+                                <div className='box-border rounded-md w-100'>
+                                    aaaaaa
+                                </div>
+                            </div>
+                            <div>
+                                sdf
+                            </div>
+                        </div>
+                        <div className='grow-0 w-full h-full'>
+                           
+                        </div>
+                        <div className='w-1/8 m-2'>
+                            This is box
+                        </div>
+                    </div>
+                </div>
+                <footer>
+                    ok
+                </footer>
+            </div>
         </div>
     )
 }
