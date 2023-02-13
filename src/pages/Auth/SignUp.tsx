66bb2c83addec996e84ca8aef773bcbf8eb7d86c
Rafael Brouwer <happinies11@gmail.com>
2023-02-13 21:32:37 +0900

    feature/ts003:Conplete designing

--- src/pages/Auth/SignUp.tsx
+++ src/pages/Auth/SignUp.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const SingUp = () => {
     const clickLogin = () => {
-
+        console.log('clicek')
     }
     return (<>
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
